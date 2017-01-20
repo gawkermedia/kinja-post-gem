ce2af04c64a182490869063ab330e16041abb4fc
Adam Pash <adam.pash@gmail.com>
2017-01-19 16:24:12 -0800

    fix: moved token to body instead of headers

--- lib/kinja/post.rb
+++ lib/kinja/post.rb
@@ -22,11 +22,11 @@ module Kinja
           defaultBlogId: opts[:defaultBlogId],
           status: opts[:status],
           allowReplies: opts[:replies],
-          tags: []
+          tags: [],
+          token: token
         }.to_json,
         headers: {
-          'content-type' => 'application/json',
-          'token' => token
+          'content-type' => 'application/json'
         }
     end
 
@@ -35,11 +35,11 @@ module Kinja
 
       id = get_post_id link_or_id
       opts[:defaultBlogId] = opts[:defaultBlogId] || get_default_blog_id(@user)
+      opts[:token] = token
       HTTParty.post update_post_path(id),
         body: opts.to_json,
         headers: {
-          'Content-Type' => 'application/json',
-          'token' => token
+          'Content-Type' => 'application/json'
         }
     end
 
--- lib/kinja/version.rb
+++ lib/kinja/version.rb
@@ -1,3 +1,3 @@
 module Kinja
-  VERSION = "0.0.22"
+  VERSION = "0.0.23"
 end
